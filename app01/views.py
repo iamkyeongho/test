e305c130d49c710d42ee61bc1806816258bac809
iamkyeongho <iamkyeongho@gmail.com>
2023-03-21 10:55:01 +0900

    pagination 수정

--- app01/views.py
+++ app01/views.py
@@ -9,8 +9,8 @@ def index(request):
     page = request.GET.get("page", '1')  # 페이지 정보를 받아오고 없으면 초기값 1
     blog_list = Blog.objects.order_by("-create_date") # order by create_date desc
     paginator = Paginator(blog_list, 10)  # 10개씩
-    page_obj = paginator.get_page(page)
-    context = {"title":"테스트", "blog_list":page_obj}
+    object_list = paginator.get_page(page)
+    context = {"title":"테스트", "object_list":object_list}
     return render(request, "app01/index.html", context)
 
 def create_blog(request):
