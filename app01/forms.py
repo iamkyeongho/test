5a0ea58ebcebdff0251a189db8b2f0d07546b7ac
iamkyeongho <iamkyeongho@gmail.com>
2023-04-05 08:21:26 +0900

    Pagination 분리

--- app01/forms.py
+++ app01/forms.py
@@ -5,3 +5,20 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ['subject', 'ctx_text', 'ctx_image']
+
+        widgets = {
+            'subject': forms.TextInput(attrs={
+                'class':'form-control',
+                'placeholder':'제목',
+            }),
+            'ctx_text': forms.Textarea(attrs={
+                'class':'form-control',
+                'rows': 10,
+                'placeholder':'내용',
+            }),
+        }
+        labels = {
+            'subject': '제목',
+            'ctx_text': '내용',
+            'ctx_image': '이미지',
+        }
